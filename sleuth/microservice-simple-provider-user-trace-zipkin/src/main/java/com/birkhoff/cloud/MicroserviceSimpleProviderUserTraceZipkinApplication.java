fe81afc362b689763f26e15e71649f8e0fdbe585
birkhoffliu <birkhoffliu@163.com>
2020-02-24 18:10:29 +0800

    init

---
+++ sleuth/microservice-simple-provider-user-trace-zipkin/src/main/java/com/birkhoff/cloud/MicroserviceSimpleProviderUserTraceZipkinApplication.java
@@ -0,0 +1,11 @@
+package com.birkhoff.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleProviderUserTraceZipkinApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceSimpleProviderUserTraceZipkinApplication.class, args);
+  }
+}
---
+++ sleuth/microservice-simple-provider-user-trace/src/main/java/com/birkhoff/cloud/MicroserviceSimpleProviderUserTraceApplication.java
@@ -0,0 +1,11 @@
+package com.birkhoff.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleProviderUserTraceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceSimpleProviderUserTraceApplication.class, args);
+  }
+}
